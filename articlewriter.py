4ebb381210c8f564f82b9700e9ed6dc92a67bcf4
PraveenM1132 <159011612+PraveenM1132@users.noreply.github.com>
2024-02-05 15:31:29 +0530

    Add files via upload

---
+++ articlewriter.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+import streamlit as st
+
+clt = OpenAI(
+    # defaults to os.environ.get("OPENAI_API_KEY")
+    api_key= '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'
+    #'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'
+    #
+    #'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'#'Your-Key'
+)
+
+def main():
+    st.title("Article Writer")
+    notes = st.text_area("Enter Topic Information:")
+    content = "I want you to write short literature review on topic "+notes
+    if st.button("Generate Article"):
+        with st.spinner("Generating Article..."):
+            response = clt.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[{'role':'user','content':content}]
+            )
+        description = response.choices[0].message.content
+        st.subheader("Generated Writeup:")
+        st.write(description)
+
+if __name__ == '__main__':
+    main()
